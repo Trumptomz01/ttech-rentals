025f58d78806bcc0cff1e4f0e4fb9a199035d3dd
Trumptomz <gbadamosioluwatomiwa441@gmail.com>
2025-02-06 19:49:28 -0800

    changes in progress...rebuilding different parts

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const passwordInput = document.getElementById('password');
-const confirmPasswordInput = document.getElementById('confirm_password');
-const errorMessage = document.getElementById('error_message');
-confirmPasswordInput.addEventListener('input',() => {
-    if (passwordInput.value !== confirmPasswordInput.value){
-        errorMessage.textContent = 'Password Not Match!';
-        // errorMessage.style.color = 'red';
-        errorMessage.style.size= '10';
-    }
-    else{
-        errorMessage.textContent = '';
-    }
-});
 
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
